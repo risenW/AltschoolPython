4f3f179be7db51429b9f297ba3505b6d774865ef
Rising Odegua <risingodegua@gmail.com>
2023-05-10 20:54:44 +0100

    project week 3

--- backend/month1/week3/project.py
+++ backend/month1/week3/project.py
@@ -22,9 +22,84 @@
 
 # We will create two classes called SavingsAccount and CurrentAccount that will inherit from the Account class.
 # The SavingsAccount class will have the following methods:
-# 1. get_account_type() - This method will override the get_account_type() method in the Account class.
 # 2. get_account_details() - This method will override the get_account_details() method in the Account class.
 
 # The CurrentAccount class will have the following methods:
-# 1. get_account_type() - This method will override the get_account_type() method in the Account class.
 # 2. get_account_details() - This method will override the get_account_details() method in the Account class.
+
+
+class Account:
+    def __init__(self, account_number, account_type, balance, account_holder):
+        self.account_number = account_number
+        self.account_type = account_type
+        self.balance = balance
+        self.account_holder = account_holder
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise ValueError("You do not have enough money!!! Thief")
+
+        self.balance -= amount
+
+    def get_balance(self):
+        return self.balance
+
+    def get_account_number(self):
+        return self.account_number
+
+    def get_account_type(self):
+        return self.account_type
+
+    def get_account_details(self):
+        return {
+            "Account Name": self.account_holder,
+            "Account Type": self.account_type,
+            "Account Number": self.account_number,
+            "Account Balance": self.balance,
+        }
+
+
+class SavingsAccount(Account):
+    def get_account_details(self):
+        return {
+            "Account Name": self.account_holder,
+            "Account Type": self.account_type,
+            "Account Number": self.account_number,
+            "Account Balance": self.balance,
+            "Extra Details": "This is an account for students",
+        }
+
+
+class CurrentAccount(Account):
+    def get_account_details(self):
+        return {
+            "Account Name": self.account_holder,
+            "Account Type": self.account_type,
+            "Account Number": self.account_number,
+            "Account Balance": self.balance,
+            "Extra Details": "This is an account for businesses",
+        }
+
+
+john = SavingsAccount(129762, "Savings", 200, "John Yemi")
+
+# print(john.get_balance())
+
+# john.deposit(200000)
+# john.deposit(3000)
+# john.deposit(1000)
+
+
+# print(john.get_balance())
+
+# john.withdraw(4200)
+
+# print(john.get_balance())
+
+
+mary = CurrentAccount(9988732, "Current", 60000, "Mary King")
+print(mary.get_balance())
+print(mary.get_account_details())
