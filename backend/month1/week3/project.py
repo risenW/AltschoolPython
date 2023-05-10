5108025de9101589449b3ec9eb272e5a49d8593f
Rising Odegua <risingodegua@gmail.com>
2023-05-10 13:30:48 +0100

    complete week 3 code

---
+++ backend/month1/week3/project.py
@@ -0,0 +1,30 @@
+# Putting all the lessons we have learnt from classes and objects together,
+# we will create a project that will help us understand the concepts better.
+
+# Project description:
+# We will create a simple banking system that will allow us to create bank accounts,
+# deposit money into the accounts, withdraw money from the accounts, and check the balance of the accounts.
+# We will create a class called Account that will serve as a template for other classes to inherit from.
+
+# The Account class will have the following properties:
+# 1. account_number - This will be a unique number that will be assigned to each account.
+# 2. account_type - This will be the type of account. It can be either savings or current.
+# 3. balance - This will be the amount of money in the account.
+# 4. account_holder - This will be the name of the person that owns the account.
+
+# The Account class will have the following methods:
+# 1. deposit(amount) - This method will be used to deposit money into the account.
+# 2. withdraw(amount) - This method will be used to withdraw money from the account.
+# 3. get_balance() - This method will be used to check the balance of the account.
+# 4. get_account_number() - This method will be used to get the account number of the account.
+# 5. get_account_type() - This method will be used to get the account type of the account.
+# 6. get_account_details() - This method will be used to get the account details of the account.
+
+# We will create two classes called SavingsAccount and CurrentAccount that will inherit from the Account class.
+# The SavingsAccount class will have the following methods:
+# 1. get_account_type() - This method will override the get_account_type() method in the Account class.
+# 2. get_account_details() - This method will override the get_account_details() method in the Account class.
+
+# The CurrentAccount class will have the following methods:
+# 1. get_account_type() - This method will override the get_account_type() method in the Account class.
+# 2. get_account_details() - This method will override the get_account_details() method in the Account class.
