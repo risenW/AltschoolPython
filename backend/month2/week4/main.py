d6528c9354e30c00a8c5e9783e06707f0ed81f47
Rising Odegua <risingodegua@gmail.com>
2023-05-12 14:13:05 +0100

    add example for cors and headers

--- backend/month2/week4/main.py
+++ backend/month2/week4/main.py
@@ -1,41 +1,27 @@
+from fastapi import FastAPI, Header, Cookie
 from typing import Annotated
-from fastapi import FastAPI, HTTPException, status, Depends
-from fastapi.security import OAuth2PasswordRequestForm
-from schema import Response
-from api.api import api_router
-from user_service import UserService
 
-app = FastAPI(
-    title="Books API",
-    description="A simple API to manage books in a library",
-    version="1.0.0",
-    docs_url="/api/v1/docs",
-)
+app = FastAPI()
 
-app.include_router(api_router)
+# FastAPI allows easy access to headers and cookies in HTTP requests.
+# Headers and cookies provide additional information and functionality in API communication.
 
 
-@app.get("/", status_code=200)
-def home():
-    return Response(message="Hello from the books API")
+@app.get("/users")
+async def users():
+    return [
+        {"id": 1, "name": "John Doe"},
+        {"id": 2, "name": "Jane Doe"},
+    ]
 
 
-@app.get("/token", status_code=status.HTTP_200_OK)
-def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    email = form_data.username
-    password = form_data.password
+@app.get("/headers")
+async def get_header(user_agent: Annotated[str | None, Header()]):
+    # Accessing headers
+    return {"User-Agent": user_agent}
 
-    user_service = UserService(users)
-    user = user_service.find_user_by_email(email)
 
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-
-    if not user_service.check_password(email, password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password"
-        )
-
-    return {"access_token": user.id, "token_type": "bearer"}
+@app.get("/cookies")
+async def endpoint_example(_ga: Annotated[str | None, Cookie()] = None):
+    # Accessing cookies
+    return {"Session-ID": _ga}
