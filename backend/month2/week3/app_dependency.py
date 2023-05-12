b02465a10152db08cc31d119504cf41d6ab5d0b8
Rising Odegua <risingodegua@gmail.com>
2023-05-12 10:11:57 +0100

    complete week 3

---
+++ backend/month2/week3/app_dependency.py
@@ -0,0 +1,31 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException
+
+app = FastAPI()
+
+
+def check_token(token: str):
+    if token != "secret":
+        raise HTTPException(status_code=403, detail="Unauthorized")
+
+
+@app.get("/items/")
+def read_items(token: Annotated[str, Depends(check_token)]):
+    print(token)
+    return {
+        "items": [
+            {"name": "Item 1"},
+            {"name": "item 2"},
+        ]
+    }
+
+
+@app.get("/users/")
+def read_users(token: Annotated[str, Depends(check_token)]):
+    return {
+        "users": [
+            {"username": "johndoe"},
+            {"username": "alice"},
+        ]
+    }
---
+++ backend/month2/week3/app_middleware.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+# The code demonstrates the use of middleware in FastAPI for logging requests and responses.
+#  - The log_middleware function is defined to handle the middleware logic.
+#  - It takes in a Request object and a call_next function that represents the next middleware or endpoint to be executed.
+#  - Inside the middleware function, you can perform actions before and after the route is executed. In this example,
+#    it logs the request method and URL before the route is called and the response status code after the route is executed.
+#  - The app.middleware("http") statement registers the middleware function to be executed for every HTTP request.
+#  - The hello_world endpoint is a simple example that returns a JSON response with a "message" key.
+
+
+def log_middleware(request: Request, call_next):
+    # Perform actions before the route is executed
+    print(f"Request received: {request.method} {request.url}")
+    response = call_next(request)
+    # Perform actions after the route is executed
+    print(f"Response sent: {response.status_code}")
+    return response
+
+
+app.middleware("http")(log_middleware)
+
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello, World!"}
