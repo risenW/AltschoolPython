1f2702ad9c6975713aea14fe71ae17934b034c19
Rising Odegua <risingodegua@gmail.com>
2023-05-17 21:33:43 +0100

    Add example for a logging and rate limiter

---
+++ backend/month3/semester2/week4/app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+limiter = Limiter(key_func=get_remote_address)
+
+app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+
+# Note: the route decorator must be above the limit decorator, not below it
+@app.get("/home")
+@limiter.limit("5/minute")
+def homepage(request: Request):
+    return {"message": "Hello Server"}
