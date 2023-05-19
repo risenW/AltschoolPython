b40be18a45fd74443e556db4cba2e8b7dae51708
Rising Odegua <risingodegua@gmail.com>
2023-05-19 14:22:36 +0100

    add withdraw feature

--- backend/month3/semester2/week4/router/accounts.py
+++ backend/month3/semester2/week4/router/accounts.py
@@ -24,7 +24,7 @@ def create_account(account_in: AccountCreate, current_user=Depends(get_current_u
     return {"message": "Account created successfully!", "data": account}
 
 
-@accounts_router.put("/")
+@accounts_router.post("/deposit/")
 def deposit_money(amount: float, current_user=Depends(get_current_user)):
     account = account_service.deposit(amount, current_user)
 
@@ -35,3 +35,16 @@ def deposit_money(amount: float, current_user=Depends(get_current_user)):
         )
 
     return {"message": "Deposit made successfully!", "data": account}
+
+
+@accounts_router.post("/withdraw/")
+def withdraw_money(amount: float, current_user=Depends(get_current_user)):
+    account = account_service.withdraw(amount, current_user)
+
+    if not account:
+        raise HTTPException(
+            status_code=401,
+            detail={"message": "An error occurred! we could not find your money"},
+        )
+
+    return {"message": "Withdraw successfully!", "data": account}
--- backend/month3/semester2/week4/services/account.py
+++ backend/month3/semester2/week4/services/account.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from schemas.account import AccountCreate, Account
 from schemas.user import UserInDB
 from database import accounts_collection
@@ -23,14 +24,34 @@ class AccountService:
 
     def deposit(self, amount: float, current_user: UserInDB):
         account = accounts_collection.find_one({"user_id": current_user.id})
-        print(account)
 
         if not account:
             return None
 
+        new_balance = account["balance"] + amount
         account = accounts_collection.find_one_and_update(
             {"_id": account["_id"]},
-            {"$set": {"balance": amount}},
+            {"$set": {"balance": new_balance}},
+            return_document=True,
+        )
+
+        return account_serializer(account)
+
+    def withdraw(self, amount: float, current_user: UserInDB):
+        account = accounts_collection.find_one({"user_id": current_user.id})
+
+        if not account:
+            return None
+
+        if account["balance"] < amount:
+            raise HTTPException(
+                status_code=401, detail={"message": "INSUFFICIENT FUNNNNDDDDSSSS"}
+            )
+
+        new_balance = account["balance"] - amount
+        account = accounts_collection.find_one_and_update(
+            {"_id": account["_id"]},
+            {"$set": {"balance": new_balance}},
             return_document=True,
         )
 
